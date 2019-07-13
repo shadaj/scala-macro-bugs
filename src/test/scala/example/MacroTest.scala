f95400f6d0fab76f215069fb0e6353f284a308a1
Shadaj Laddad <shadaj@users.noreply.github.com>
2019-07-12 17:04:14 -0700

    Improve naming

--- src/test/scala/example/MacroTest.scala
+++ src/test/scala/example/MacroTest.scala
@@ -2,6 +2,6 @@ package example
 
 object MacroTest {
   def a(default: Int = 0, notDefault: Int)(args: Int*) = ???
-  def b(str: => Int): Int = ???
+  def b(num: => Int): Int = ???
   a(notDefault = 0)(b(Macro.myMacro()))
 }
